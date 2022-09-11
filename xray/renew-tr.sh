d0b20584de1e93a5de51e54b7bec6a2909fe5c88
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-09-11 11:26:33 +0700

    Update renew-tr.sh

--- xray/renew-tr.sh
+++ xray/renew-tr.sh
@@ -10,25 +10,6 @@ PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
-# Getting
-MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-IZIN=$( curl ipinfo.io/ip | grep $MYIP )
-if [ $MYIP = $MYIP ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
-else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-echo -e "${NC}${LIGHT}Fuck you!!"
-exit 0
-fi
-clear
-NUMBER_OF_CLIENTS=$(grep -c -E "^#### " "/etc/xray/config.json")
-	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
-		clear
-		echo ""
-		echo "You have no existing clients!"
-		exit 1
-	fi
 
 	clear
 	echo ""
